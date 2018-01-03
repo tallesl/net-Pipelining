aaad6e305b185af8ea30ec39883b2f1d71811386
Talles L <talleslasmar@gmail.com>
2018-01-03 16:20:31 -0200

    import tests

---
+++ UnitTests/ImportTests.cs
@@ -0,0 +1,34 @@
+﻿namespace PipeliningLibrary.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ImportTests
+    {
+        [TestMethod]
+        public void Import()
+        {
+            var expected = "cowabunga";
+            var pipelineGroup = new PipelineGroup();
+
+            pipelineGroup.Import(new TextPipelines());
+
+            var actual = pipelineGroup["sanitize_input"].Run("Cowabunga!");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ImportTypeT()
+        {
+            var expected = "cowabunga";
+            var pipelineGroup = new PipelineGroup();
+
+            pipelineGroup.Import<TextPipelines>();
+
+            var actual = pipelineGroup["sanitize_input"].Run("Cowabunga!");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
